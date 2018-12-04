5d6b4705c84ba633503e14cdf7b70902e31142df
Scott Geier <scgeier@gmail.com>
2018-12-03 22:43:27 -0500

    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
 
   console.log('scripts loaded');
 
-
-
-
-
    var tag = document.createElement('script');
 
         tag.src = "https://www.youtube.com/iframe_api";
         var firstScriptTag = document.getElementsByTagName('script')[0];
         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+
         var player;
         var player2;
+//Create all of the videos once the API is ready
+//TAll of the players must be inside this function
         function onYouTubeIframeAPIReady() {
           player = new YT.Player('grinch-vid', {
             height: '390',
@@ -34,30 +33,32 @@
             }
           });
 
+//Show the Grinch video and hide/stop the others
           $('#grinch').click(function(){
             $('#grinch-vid').show();
             $('#sia-vid').hide();
             player.playVideo();
             player2.stopVideo();
           });
-
+//Show the Sia video and hide/stop the others
             $('#sia').click(function(){
-              //Change the chosenId to the Sia video
+
               $('#sia-vid').show();
-              $('#grinch-vid').hide();
               player2.playVideo();
               player.stopVideo();
-
+              $('#grinch-vid').hide();
             });
         }
 
-
-
+        // 4. The API will call this function when the video player is ready.
         function onPlayerReady(event) {
           event.target.loadVideo();
         }
 
-
+        // 5. The API calls this function when the player's state changes.
+        //    The function indicates that when playing a video (state=1),
+        //    the player should play for six seconds and then stop.
+        var done = false;
         function onPlayerStateChange(event) {
           if (event.data == YT.PlayerState.PLAYING && !done) {
             setTimeout(stopVideo, 6000);
@@ -67,14 +68,3 @@
         function stopVideo() {
           player.stopVideo();
         }
-
-       var done = false;
-       function onPlayerStateChange(event) {
-         if (event.data == YT.PlayerState.PLAYING && !done) {
-           setTimeout(stopVideo, 6000);
-           done = true;
-         }
-       }
-       function stopVideo() {
-         player.stopVideo();
-       }
